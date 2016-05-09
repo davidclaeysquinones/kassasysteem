9c207aa904e35480abce9728dba6f6de091bc959
davidclaeysquinones <davidclaeys@outlook.com>
2016-05-09 15:31:45 +0200

    dds

--- KassaSysteem/Kassa.xaml.cs
+++ KassaSysteem/Kassa.xaml.cs
@@ -10,20 +10,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace KassaSysteem
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for Kassa.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class Kassa : Window
     {
-        public MainWindow()
+        public Kassa()
         {
             InitializeComponent();
         }
-
     }
 }
