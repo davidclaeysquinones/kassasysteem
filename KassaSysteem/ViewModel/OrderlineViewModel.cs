72343f2cb5b20d4d1ae9c88cc9f4b6ba293042cc
AnthonyBroeckaert <r0464343@student.vives.be>
2016-05-18 20:07:22 +0200

    tt

---
+++ KassaSysteem/ViewModel/OrderlineViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KassaSysteem.ViewModel
+{
+    public class OrderlineViewModel
+    {
+        public int Id { get; set; }
+        public int OrderId { get; set; }
+        public int ArticleId { get; set; }
+        public string ArticleName { get; set; }
+        public float Price { get; set; }
+        public int Amount { get; set; }
+        public float Total { get; set; }
+        public System.DateTime CreatedDate { get; set; }
+
+    }
+}
